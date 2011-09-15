e7c6503fd6c68a791055082c02d7f28d06bca051
Matthias Siegel <matthiassiegel@gmail.com>
2011-09-15 13:21:12 +0930

    Javascripts improved

--- scripts.js
+++ scripts.js
@@ -1,19 +1,21 @@
 jQuery(document).ready(function($) {
 	
 	$('#related-posts-select').change(function() {
-		var container = $('#related-posts');
-		var id = $(this).val();
-		var title = this.options[this.options.selectedIndex].text;
+		var select = $(this),
+				container = $('#related-posts'),
+				id = select.val(),
+				title = this.options[this.options.selectedIndex].text;
 		
 		if ($('#related-post-' + id).length == 0) {
-			container.prepend('<div class="related-post" id="related-post-' + id + '"><input type="hidden" name="related-posts[]" value="' + id + '" /><span class="related-post-title">' + title + '</span><a href="#">Delete</a></div>');
+			container.prepend('<div class="related-post" id="related-post-' + id + '"><input type="hidden" name="related-posts[]" value="' + id + '"><span class="related-post-title">' + title + '</span><a href="#">Delete</a></div>');
 		}
 	});
 	
 	$('.related-post a').live('click', function() {
-		$(this).parent().css('background-color', '#ff0000');
-		$(this).parent().fadeOut('normal', function() {
-			$(this).remove();
+		var div = $(this).parent();
+		
+		div.css('background-color', '#ff0000').fadeOut('normal', function() {
+			div.remove();
 		});
 		return false;
 	});
